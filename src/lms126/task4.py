c02021df9b26d9d5562e797fa18b8115f3ddecf5
Magnuspython <122862898+Magnuspython@users.noreply.github.com>
2023-02-07 10:08:58 +0100

    Solution task 4

--- src/lms126/task4.py
+++ src/lms126/task4.py
@@ -2,4 +2,11 @@ from to_do import TODO
 
 
 def task4():
-    return TODO("Erase this line and write your solution")
+    return """
+    Take two numbers (variables) X and Y and give them numbers: 5 and 8
+    If X is greater than Y output X as a larger number.
+    If Y is greater then X output Y as a larger number.   
+
+  
+    
+    """
