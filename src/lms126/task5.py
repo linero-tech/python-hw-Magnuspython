44bf36e627f459b92b5089078ebf5fbe7f1f70c3
Magnuspython <122862898+Magnuspython@users.noreply.github.com>
2023-02-07 10:24:08 +0100

    solution task 5

--- src/lms126/task5.py
+++ src/lms126/task5.py
@@ -2,4 +2,14 @@ from to_do import TODO
 
 
 def task5():
-    return TODO("Erase this line and write your solution")
+    return """
+    input X 
+    if x is number 1 print monday
+    if x is number 2 print tuesday
+    if x is number 3 print wednesday
+    if x is number 4 print thursday
+    if x is number 5 print friday
+    if x is number 6 print saturday
+    if x is number 7 print sunday
+    
+    """
