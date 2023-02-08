a39153e61d518f3c1ffcc7432b0272d9bdbcb1c5
Magnuspython <122862898+Magnuspython@users.noreply.github.com>
2023-02-08 20:12:27 +0100

    Solution mod 102 task 1

--- src/lms127/task1.py
+++ src/lms127/task1.py
@@ -2,4 +2,9 @@ from to_do import TODO
 
 
 def task1():
-    TODO("Erase this line and write your solution")
+    print("Country: Sweden")
+    print("Capital: Stockholm")
+    print("Currency: SEK")
+
+if __name__ == "__main__":
+        task1()
