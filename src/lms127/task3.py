0cbef4f1bd7a8d2366c1a8d1f08cfa5d3f474305
Magnuspython <122862898+Magnuspython@users.noreply.github.com>
2023-02-13 10:20:04 +0100

    Solution mod 102 task 3

--- src/lms127/task3.py
+++ src/lms127/task3.py
@@ -1,7 +1,14 @@
 from to_do import TODO
 
 
-def task3(radius):
-    return TODO(
-        "Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword"
-    )
+def task3():
+    PI = 3.14
+    R = float(input('radius of circle:'))
+    perimeter = (2 * PI * R)
+    print('result is', perimeter)
+
+
+if __name__ == "__main__":
+    task3()
+
+
