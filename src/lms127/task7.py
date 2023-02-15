e0f0d34e8db4c47f762cc9203c302acc1a2aa2b5
Magnuspython <122862898+Magnuspython@users.noreply.github.com>
2023-02-15 08:24:46 +0100

    Solution mod 102 task 7

--- src/lms127/task7.py
+++ src/lms127/task7.py
@@ -1,7 +1,8 @@
 from to_do import TODO
 
 
-def task7(sentence):
-    return TODO(
-        "Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword"
-    )
+def task7():
+    sentence = (input('write an sentence to count the characters:'))
+    print("result is", len(sentence))
+if __name__ == "__main__":
+    task7()
