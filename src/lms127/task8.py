8025e19c5e4f77b0384b1fe534c09ccfaad2c8ea
Magnuspython <122862898+Magnuspython@users.noreply.github.com>
2023-02-15 11:58:05 +0100

    Solution mod 102 task 8

--- src/lms127/task8.py
+++ src/lms127/task8.py
@@ -1,7 +1,14 @@
 from to_do import TODO
 
 
-def task8(sentence, character):
-    return TODO(
-        "Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword"
-    )
+def task8():
+    sentence = input('Write an sentence here to count a chooseble character:')
+    character = input('Enter a character to count if how many time it occours in the sentence:')
+    count = 0
+    for x in sentence:
+        if x == character:
+            count += 1
+    print(character, 'occurs', count, 'times')
+
+if __name__ == "__main__":
+        task8()
