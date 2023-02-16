ce53c99d203e6c351f94f64a026ab8edd88341b9
Magnuspython <122862898+Magnuspython@users.noreply.github.com>
2023-02-16 09:07:38 +0100

    Solution mod 102 task 6

--- src/lms127/task6.py
+++ src/lms127/task6.py
@@ -1,12 +1,11 @@
 from to_do import TODO
 
 
-def task6():
-    b1 = float(input('input base 1:'))
-    b2 = float(input('input base 2:'))
-    h = float(input('input height:'))
-    area = (b1 * h + b2 * h) / 2
-    print('result is = ', area)
+def task6(base1, base2, height):
+    result = (base1 * height + base2 * height) / 2
+    return result
 
 if __name__ == "__main__":
-    task6()
+    print(task6(base1=10.0, base2=20.0, height=1.0))
+
+    # (b1 * h + b2 * h) / 2
