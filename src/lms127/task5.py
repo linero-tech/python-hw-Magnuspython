37f34676d3a8d6d01efe5b6f01ac58c09ef7a50f
Magnuspython <122862898+Magnuspython@users.noreply.github.com>
2023-02-20 21:07:22 +0100

    Solution mod 102 task 5

--- src/lms127/task5.py
+++ src/lms127/task5.py
@@ -1,11 +1,13 @@
-from to_do import TODO
+
 
 
 def task5(value_for_a, value_for_b):
     a = value_for_a
     b = value_for_b
+    a, b = b, a
 
-    TODO("Your solutions goes in here.")
+    return a,b
 
-    # Do not erase or change the below statement
-    return a, b
+
+if __name__ == "__main__":
+    print (task5(1, 2))
