a4dd3cc8ede3a9d98cd8b0fb4f5e141b55ae603e
Magnuspython <122862898+Magnuspython@users.noreply.github.com>
2023-03-03 10:18:03 +0100

    sol mod 103 task 3

--- src/lms129/task3.py
+++ src/lms129/task3.py
@@ -2,6 +2,14 @@ from to_do import TODO
 
 
 def task3(number):
-    return TODO(
-        "Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword"
-    )
+    if number == 1:
+        return 1
+    else:
+        return number * task3(number - 1)
+
+
+
+if __name__ == "__main__":
+    print("result is", task3(5))
+
+
