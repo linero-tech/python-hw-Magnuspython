147428bb76a267b927f5efaea536cf2624e9f9c6
Magnuspython <122862898+Magnuspython@users.noreply.github.com>
2023-03-03 10:28:50 +0100

    sol mod 103 task 1

--- src/lms129/task1.py
+++ src/lms129/task1.py
@@ -2,6 +2,15 @@ from to_do import TODO
 
 
 def task1(a, b):
-    return TODO(
-        "Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword"
-    )
+   result = a, b
+   for var in result:
+        if a >= b:
+            result = 0
+        else:
+            result = f"{a}{b}"
+
+        return result
+
+
+if __name__ == "__main__":
+    print(task1(1, 5))
