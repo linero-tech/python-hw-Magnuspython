9e47e5357356ed23965903418a7f78e49e360998
Magnuspython <122862898+Magnuspython@users.noreply.github.com>
2023-03-03 11:53:39 +0100

    sol 103 task 5

--- src/lms129/task5.py
+++ src/lms129/task5.py
@@ -2,6 +2,13 @@ from to_do import TODO
 
 
 def task5(sentence):
-    return TODO(
-        "Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword"
-    )
+    result = 0
+    for i in sentence:
+        result += 1
+
+
+    return result
+
+
+if __name__ == "__main__":
+    print("result is", task5("i love gbg"))
