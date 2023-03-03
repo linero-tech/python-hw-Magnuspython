bcd01f08b91b96d52f7f08afa4d02d0892b592ce
Magnuspython <122862898+Magnuspython@users.noreply.github.com>
2023-03-03 10:33:18 +0100

    sol mod 103 task 2

--- src/lms129/task2.py
+++ src/lms129/task2.py
@@ -2,6 +2,18 @@ from to_do import TODO
 
 
 def task2(number):
-    return TODO(
-        "Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword"
-    )
+    result = is_prime = True
+    if number > 1:
+
+
+        for divisor in range(2, number):
+            if (number % divisor) == 0:
+                result = False
+                break
+            else:
+                is_prime = True
+
+    return result
+
+if __name__ == "__main__":
+    print(task2(5))
