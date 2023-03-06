6cfd2601801ed312bd73086d663a539ec7dbbacc
Magnuspython <122862898+Magnuspython@users.noreply.github.com>
2023-03-06 09:51:25 +0100

    sol 103 task 8

--- src/lms129/task8.py
+++ src/lms129/task8.py
@@ -2,6 +2,14 @@ from to_do import TODO
 
 
 def task8(number):
-    return TODO(
-        "Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword"
-    )
+
+    result = 0
+    for digit in str(number):
+        result += int(digit)
+    return result
+
+if __name__ == "__main__":
+    print("result is", task8(123))
+
+
+# https://www.geeksforgeeks.org/python-program-for-sum-the-digits-of-a-given-number/
