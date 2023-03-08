c65b2ce9e950b4ed0c806c75c9f317dab74bd58f
Magnuspython <122862898+Magnuspython@users.noreply.github.com>
2023-03-08 09:48:59 +0100

    sol mod 102 task 10

--- src/lms127/task10.py
+++ src/lms127/task10.py
@@ -2,24 +2,29 @@ from to_do import TODO
 
 
 def task10_1(assessments):
-    return TODO(
-        "Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword"
-    )
+    return len(assessments)
+
 
 
 def task10_2(assessments):
-    return TODO(
-        "Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword"
-    )
+    return assessments[3]
+
 
 
 def task10_3(assessments):
-    return TODO(
-        "Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword"
-    )
+    number_of_assessments = len(assessments)
+    index_of_middle = int(number_of_assessments / 2)
+    middle_assessment = assessments[index_of_middle]
+    return middle_assessment
+# return assessments[len(assessments) // 2]
+
+
 
 
 def task10_4(assessments):
-    return TODO(
-        "Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword"
-    )
+    result = assessments[0:3]
+    return result
+
+
+if __name__ == "__main__":
+    print(task10_4("LMFHM"))
