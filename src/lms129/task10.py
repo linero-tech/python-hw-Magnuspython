cb2128c166019a1f3694ebea7ba2ca7e49711208
Magnuspython <122862898+Magnuspython@users.noreply.github.com>
2023-03-14 09:51:51 +0100

    sol mod 103 task 10

--- src/lms129/task10.py
+++ src/lms129/task10.py
@@ -2,6 +2,13 @@ from to_do import TODO
 
 
 def task10(password):
-    return TODO(
-        "Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword"
-    )
+    has_lowercase = False
+    has_special_character = False
+    has_uppercase = False
+    has_number = False
+    has_correct_length = len(password) in range(6, 11)
+
+    return has_lowercase and has_special_character and has_uppercase and has_number and has_correct_length
+
+if __name__ == "__main__":
+    print(task10(password= "abc"))
